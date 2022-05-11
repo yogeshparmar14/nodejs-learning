910684a7bf8cf0ac311e7b9b39aea22eba5e3079
yogesh parmar <mr.yoraj@gmail.com>
2022-05-11 17:38:29 +0530

    9th comment

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const router = require("./controller/routes.js");
 app.use(express.urlencoded({extended:true}));
 app.use("/statics",express.static(__dirname + "/statics"));
 
-app.set("view engines","ejs");
+app.set("view engine","ejs");
 app.set("views",__dirname + "/views");
 
 app.use("/accounts",router);
 
-app.use("/:name?",(req,res)=>{
-   var name = req.params.name;
-   if(name){
-   res.render("index.ejs",{name: name});}
-   else{
-    res.render("index.ejs");
-   }
+app.get("/",(req,res)=>{
+    
+    res.render("index");
+   
 });
+
+app.get("/contact",(req, res)=>{
+    res.render("contact")
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
