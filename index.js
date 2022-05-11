c99848602a01b9543ea3a79ca6cf533da4813bb9
yogesh parmar <mr.yoraj@gmail.com>
2022-05-11 16:09:12 +0530

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const router = require("./controller/routes.js");
 
 
 app.use(express.urlencoded({extended:true}));
-app.use("/statics",express.static(__dirname + "/statics"))
+app.use("/statics",express.static(__dirname + "/statics"));
+
 app.use("/accounts",router);
+
+app.use("/:name",(req,res)=>{
+   var name = req.params.name;
+   res.send(name);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
